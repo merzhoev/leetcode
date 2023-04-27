e437c2fa39b94bfb2749fbe34b4d34b109185b84
merzhoev <arbimerzhoev@gmail.com>
2023-04-27 10:55:42 +0300

    2627. Debounce

---
+++ medium/2627_debounce.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/debounce/
+
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function (fn, t) {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(fn, t, ...args)
+  }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */

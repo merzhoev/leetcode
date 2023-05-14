898b34c3e24a727a91e1c6329ffe2ae3523936c0
merzhoev <arbimerzhoev@gmail.com>
2023-05-14 14:33:50 +0300

    217. Contains Duplicate

---
+++ easy/217_contains-duplicate.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/contains-duplicate/
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicate = (nums) => {
+  const set = new Set()
+
+  for (const num of nums) {
+    if (set.has(num)) {
+      return true
+    }
+
+    set.add(num)
+  }
+
+  return false
+};

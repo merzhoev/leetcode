fdabe185b84cd0b13fdeccbbf9d5c07f012820ab
merzhoev <arbimerzhoev@gmail.com>
2023-05-15 09:39:59 +0300

    215. Kth Largest Element in an Array

---
+++ medium/215_kth-largest-element-in-an-array.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const findKthLargest = (nums, k) => {
+  return nums.sort((a, b) => a - b)[nums.length - k]
+};
